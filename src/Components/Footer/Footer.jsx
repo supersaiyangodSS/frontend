f95c45ff7ad5c5fa950ce0d469a2d7af38e46e09
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-08-02 15:00:17 +0530

    name edit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
     <div className='footer'>
       <div className="footer-logo">
         <img src={footer_logo} alt="" />
-        <p>SHOPPER</p>
+        <p>Beunique</p>
       </div>
       <ul className="footer-links">
         <li>Company</li>
@@ -32,7 +32,7 @@ const Footer = () => {
       </div>
       <div className="footer-copyright">
         <hr />
-        <p>Copyright @ 2023 - All Right Reserved.</p>
+        <p>&copy; Copyright 2024 - Beunique All Right Reserved.</p>
       </div>
     </div>
   )
